89a12648ae3b7b71003d48155aeed5b3b232f5b6
AbhisGitHub <abhijitdey217@gmail.com>
2020-11-19 16:32:15 +0530

    2 files modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -1,5 +1,11 @@
 import { Component } from "@angular/core";
-import { NavController } from "ionic-angular";
+import { async } from "@angular/core/testing";
+import {
+  NavController,
+  ToastController,
+  LoadingController
+} from "ionic-angular";
+import { Observable } from "rxjs";
 import { Contact } from "../contact";
 @Component({
   selector: "page-contact",
@@ -7,8 +13,35 @@ import { Contact } from "../contact";
 })
 export class ContactPage {
   contact: Contact;
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private toastctrl: ToastController,
+    private loadingctrl: LoadingController
+  ) {
     this.contact = new Contact();
   }
-  SaveCont(contform) {}
+
+  async presentLoading() {
+    const loader = await this.loadingctrl.create({
+      content: "Saving..."
+    });
+    await loader.present();
+    await this.presentToast().then(val => {
+      loader.dismiss();
+    });
+  }
+
+  async presentToast() {
+    const toast = await this.toastctrl.create({
+      message: "Data Saved..",
+      duration: 3000
+    });
+    await toast.present();
+  }
+
+  SaveCont(contform) {
+    //console.log(contform.value);
+    this.presentLoading();
+    contform.reset();
+  }
 }
